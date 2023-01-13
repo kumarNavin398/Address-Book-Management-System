6827d3f066a24afa86188f86203b6064a66ad6f3
KumarNavin <navins398@gmail.com>
2023-01-13 21:44:24 +0530

    Delete Contact

--- src/com/bridgelabz/AddressBookSystem.java
+++ src/com/bridgelabz/AddressBookSystem.java
@@ -3,16 +3,16 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class AddressBookSystem
-{
-    //main function
-    public static void main(String[] args) {
+{ //main method
+    public static void main(String[] args)
+    {
         AddressBook addressBook = new AddressBook();
         Scanner sc = new Scanner(System.in);
         int flag = 1 ;
         while(flag == 1)
         {
             System.out.println(" Welcome to address book program ");
-            System.out.println(" Select a choice : 1. Add 2.Edit  3. Exit");
+            System.out.println(" Select a choice : 1. Add 2.Edit  3. Delete 4. Exit");
             int choice = sc.nextInt();
             switch(choice)
             {
@@ -25,7 +25,14 @@ public class AddressBookSystem
                 }
                     addressBook.editContact();
                     break;
-                case 3 : flag = 0 ;
+                case 3:
+                    if (addressBook.contactList.isEmpty())
+                    {
+                        System.out.println(" Address book is empty ");
+                        break;
+                    }
+                    addressBook.deleteContact();
+                case 4 : flag = 0 ;
                     break;
                 default: System.out.println(" Enter a valid choice");
                     break;
@@ -33,4 +40,5 @@ public class AddressBookSystem
         }
         System.out.println(addressBook.contactList);
     }
+
 }
